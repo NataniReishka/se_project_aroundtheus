f63e56c34b3643dc9be6b7a476b1725c22456e3f
Michael Cain <kilaheneva@gmail.com>
2024-05-12 19:42:16 -0500

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ const initialCards = [
     },
 ]
 
+// Elements
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-modal");
 const profileCloseButton = document.querySelector("#modal-close-button");
@@ -34,27 +35,31 @@ const profileTitleInput = document.querySelector('#profile-title-input');
 const profileDescriptionInput = document.querySelector("#profile-description-input");
 const profileEditForm = profileEditModal.querySelector('.modal__form');
 
+// Functions 
 function closePopup () {
     profileEditModal.classList.remove("modal_opened");
 }
 
+// Event Handlers
+
+function handleProfileEditSubmit (e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup ();
+}
+
+
+// Event Listeners
 profileEditButton.addEventListener('click', () => {
     profileTitleInput.value = profileTitle.textContent;
     profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add("modal_opened");
 });
 
-profileCloseButton.addEventListener("click", () => {
-    closePopup ();
-}
-);
+profileCloseButton.addEventListener("click", closePopup);
 
-profileEditForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    profileTitle.textContent = profileTitleInput.value;
-    profileDescription.textContent = profileDescriptionInput.value;
-    closePopup ();
-}
-);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
 
